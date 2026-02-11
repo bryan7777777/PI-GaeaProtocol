18ac96e6eb2ffc7404985d273938693f7fb248e4
bryan7777777 <bryannagano15@gmail.com>
2026-02-11 17:25:31 -0300

    add conquistas

---
+++ js/jogo/arreys/conquistas.js
@@ -0,0 +1,26 @@
+const conquistas = [
+    {
+        name: 'Líder',
+        concluido: 0,
+        premio: 'magnolia',
+        dsc: 'Coloque no deck uma carta do tipo TERRA.'
+    },
+    {
+        name: 'Perdido',
+        concluido: 0,
+        premio: 'wallace',
+        dsc: 'Em uma única run, compre 5 cartas da loja.'
+    },
+    {
+        name: 'Amigavel',
+        concluido: 0,
+        premio: 'gaeaReen',
+        dsc: 'Desbloqueie todos os personagens.'
+    },
+    {
+        name: 'Águas mansas',
+        concluido: 0,
+        premio: 'porto',
+        dsc: 'Ganhe do boss Prj.Krakem uma vez.'
+    },
+]
