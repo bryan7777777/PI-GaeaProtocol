664c3fda8bea0e0c928ba80d4538bfe06230eaab
bryan7777777 <bryannagano15@gmail.com>
2026-02-12 14:02:54 -0300

    up conquistas

--- js/jogo/arreys/conquistas.js
+++ js/jogo/arreys/conquistas.js
@@ -3,30 +3,77 @@ const conquistas = [
         name: 'Líder',
         concluido: 0,
         premio: 'magnolia',
-        dsc: 'Coloque no deck uma carta do tipo TERRA.'
+        personagem: 'Magnolia',
+        dsc: 'Derrote o boss Alfa.'
     },
     {
         name: 'Perdido',
         concluido: 0,
         premio: 'wallace',
+        personagem: 'Wallace',
         dsc: 'Em uma única run, compre 5 cartas da loja.'
     },
     {
         name: 'Amigavel',
         concluido: 0,
         premio: 'gaeaReen',
+        personagem: 'Elize',
         dsc: 'Desbloqueie todos os personagens.'
     },
     {
         name: 'Mineração',
         concluido: 0,
-        premio: 'gaeaReen',
+        premio: 'amarelo',
+        personagem: 'Bruno',
         dsc: 'Pegue uma vez a carta Mineração Sustentavel.'
+    },
+    {
+        name: 'Casca grossa',
+        concluido: 0,
+        premio: 'felipe',
+        personagem: 'Felipe',
+        dsc: 'Consiga 200 de escudo em uma run.'
+    },
+    {
+        name: 'Solo áspero',
+        concluido: 0,
+        premio: 'celso',
+        personagem: 'Celso',
+        dsc: 'Use uma carta de marção de TERRA.'
+    },
+    {
+        name: 'Terroso',
+        concluido: 0,
+        premio: 'maria',
+        personagem: 'Maria',
+        dsc: 'Tenha cinco ou mais cartas do tipo TERRA no deck.'
     },
     {
         name: 'Águas mansas',
         concluido: 0,
         premio: 'porto',
-        dsc: 'Ganhe do boss Prj.Krakem uma vez.'
+        personagem: 'Clarice',
+        dsc: 'Use uma carta de marção de ÁGUA.'
+    },
+    {
+        name: 'Protetor',
+        concluido: 0,
+        premio: 'azul',
+        personagem: 'Reinor',
+        dsc: 'Use uma carta de DEFESA/AZUL.'
+    },
+    {
+        name: 'Águas turbulentas',
+        concluido: 0,
+        premio: 'fergus',
+        personagem: 'Fergus',
+        dsc: 'Derrote o boss Prj.Krakem uma vez.'
+    },
+    {
+        name: 'Águas turbulentas',
+        concluido: 0,
+        premio: 'mercuri',
+        personagem: 'Mercuri',
+        dsc: 'Tenha cinco ou mais cartas do tipo ÁGUA no deck.'
     },
 ]
