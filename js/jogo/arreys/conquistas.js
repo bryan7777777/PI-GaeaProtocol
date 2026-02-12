9828ad7b62c5ea77bd0489a85bafb82ef1688d7f
Nagano <bryannagano15@gmail.com>
2026-02-12 15:29:01 -0300

    add all qst
    add tds as conquistas dos personagens

--- js/jogo/arreys/conquistas.js
+++ js/jogo/arreys/conquistas.js
@@ -20,6 +20,9 @@ const conquistas = [
         personagem: 'Elize',
         dsc: 'Desbloqueie todos os personagens.'
     },
+
+
+
     {
         name: 'Mineração',
         concluido: 0,
@@ -42,12 +45,15 @@ const conquistas = [
         dsc: 'Use uma carta de marção de TERRA.'
     },
     {
-        name: 'Terroso',
+        name: 'Reciclador nato',
         concluido: 0,
         premio: 'maria',
         personagem: 'Maria',
-        dsc: 'Tenha cinco ou mais cartas do tipo TERRA no deck.'
+        dsc: 'Em uma única run Recicle um total de 400 lixos.'
     },
+
+
+
     {
         name: 'Águas mansas',
         concluido: 0,
@@ -60,7 +66,7 @@ const conquistas = [
         concluido: 0,
         premio: 'azul',
         personagem: 'Reinor',
-        dsc: 'Use uma carta de DEFESA/AZUL.'
+        dsc: 'Use uma carta do tipo DEFESA/AZUL.'
     },
     {
         name: 'Águas turbulentas',
@@ -70,10 +76,134 @@ const conquistas = [
         dsc: 'Derrote o boss Prj.Krakem uma vez.'
     },
     {
-        name: 'Águas turbulentas',
+        name: 'Amor pelo mar',
         concluido: 0,
         premio: 'mercuri',
         personagem: 'Mercuri',
-        dsc: 'Tenha cinco ou mais cartas do tipo ÁGUA no deck.'
+        dsc: 'Tenha cinco ou mais cartas do tipo ÁGUA no mesmo deck.'
     },
+
+
+
+    {
+        name: 'Brutal',
+        concluido: 0,
+        premio: 'laranja',
+        personagem: 'Gabriel',
+        dsc: 'Cause em um único ataque 20 de dano.'
+    },
+    {
+        name: 'Lataria Polida',
+        concluido: 0,
+        premio: 'cleber',
+        personagem: 'Cleber',
+        dsc: 'Consiga 300 de escudo.'
+    },
+    {
+        name: 'Sortudo',
+        concluido: 0,
+        premio: 'malaquias',
+        personagem: 'Malaquias',
+        dsc: 'Tenha no deck a carta Compra Dupla.'
+    },
+    {
+        name: 'Energia cortante',
+        concluido: 0,
+        premio: 'renata',
+        personagem: 'Renata',
+        dsc: 'Pegue a carta Chuva De Laminas uma vez.'
+    },
+
+
+
+    {
+        name: 'Bombeiro nato',
+        concluido: 0,
+        premio: 'marcos',
+        personagem: 'Marcos',
+        dsc: 'Tenha dez ou mais cartas do tipo ÁGUA no mesmo deck.'
+    },
+    {
+        name: 'Lenda Viva',
+        concluido: 0,
+        premio: 'cleide',
+        personagem: 'Cleide',
+        dsc: 'Tenha uma carta de cada elemento no deck.'
+    },
+    {
+        name: 'BOM!!!',
+        concluido: 0,
+        premio: 'magna',
+        personagem: 'Magna',
+        dsc: 'Tenha no deck a carta Fogo Amigo.'
+    },
+    {
+        name: 'Lenda lendaria',
+        concluido: 0,
+        premio: 'lucius',
+        personagem: 'Lucius',
+        dsc: 'Tenha cinco ou mais cartas da raridade Lendaria no mesmo deck.'
+    },
+
+
+
+    {
+        name: 'Arsenal Frio',
+        concluido: 0,
+        premio: 'lilis',
+        personagem: 'Lilis',
+        dsc: 'Pegue uma carta de GELO.'
+    },
+    {
+        name: 'Em uma fria',
+        concluido: 0,
+        premio: 'glacia',
+        personagem: 'Glacia',
+        dsc: 'Use uma carta de marcação de GELO.'
+    },
+    {
+        name: 'Coração Frio',
+        concluido: 0,
+        premio: 'olaf',
+        personagem: 'Olaf',
+        dsc: 'Tenha cinco ou mais cartas do tipo GELO no mesmo deck.'
+    },
+    {
+        name: 'Nevasca',
+        concluido: 0,
+        premio: 'olga',
+        personagem: 'Olga',
+        dsc: 'Tenha dez ou mais cartas do tipo GELO no mesmo deck.'
+    },
+
+
+
+    {
+        name: 'Neblina',
+        concluido: 0,
+        premio: 'x',
+        personagem: '#####',
+        dsc: 'Derrote o boss Nemora.'
+    },
+    {
+        name: 'Limpando a poeira',
+        concluido: 0,
+        premio: 'tomoeh',
+        personagem: 'Tomoeh',
+        dsc: 'Derrote o boss Ayla.'
+    },
+    {
+        name: 'Reciclando o amanhã',
+        concluido: 0,
+        premio: 'ferrus',
+        personagem: 'Magnus',
+        dsc: 'Tenha cinco ou mais cartas do tipo RECICLAGEM/AMARELO no mesmo deck.'
+    },
+    {
+        name: 'Reciclando o amanhã',
+        concluido: 0,
+        premio: 'roxo',
+        personagem: 'vinicius',
+        dsc: 'Tenha em uma única run 10 ou mais de energia.'
+    }
 ]
