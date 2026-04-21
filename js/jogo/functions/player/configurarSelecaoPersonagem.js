e9e7bea9816f062bd02a0ec507b9e71e810e20fa
bryan7777777 <bryannagano25@gmail.com>
2026-04-21 15:03:46 -0300

    add cleopatra lor

--- js/jogo/functions/player/configurarSelecaoPersonagem.js
+++ js/jogo/functions/player/configurarSelecaoPersonagem.js
@@ -66,13 +66,13 @@ function configurarSelecaoPersonagem() {
         },
         {
           img: "./../img/jogo/itens/peluciaDeGaea.png",
-          titulo: "GÆA",
+          titulo: "Pelucia",
           desc: "Inicia com o item \"Gaeazinha\"",
           cor: "lime"
         },
         {
           img: "./../img/jogo/itens/coroaDeGaea.png",
-          titulo: "GÆA",
+          titulo: "Coroada",
           desc: "Inicia com o item \"Coroa Da Gaea\"",
           cor: "lime"
         }
@@ -273,7 +273,7 @@ function configurarSelecaoPersonagem() {
         {
           img: "./../img/jogo/passivas/ferrus11.png",
           titulo: "Gigante De Ferro",
-          desc: "Magnus possui um mecha protetor.<br>Ele tem uma armadura inicia bem alta devido ao seu guardião.",
+          desc: "Magnus possui um mecha protetor.<br>Ele tem uma armadura inicial bem alta devido ao seu guardião.",
           cor: "lime"
         },
         {
@@ -288,7 +288,7 @@ function configurarSelecaoPersonagem() {
       nome: "🎨Mayumi, A criadoura caida🎨",
       dificuldade: "🎨NÍVEL DE DIFICULDADE: ☠️☠️☠️☠️☠️🎨",
       cor: "rgb(141, 7, 231)",
-      descricao: "Uma pintora talentosa e uma arqueira nata, deck focado entroca de estilos e mistura de cores. Sua maior força também é sua maior fraqueza, Mayumi sofre de daltonismo, especificamente acromatopsia, um tipo extremamente raro, acredita-se que ela viveu no Período 600–1000 d.C. (período Asuka a Heian, Japão), não se sabe ao certo, será que isso lhe impede de ver o fim?",
+      descricao: "Uma pintora talentosa e uma arqueira nata, deck focado em trocar de estilos e mistura de cores. Sua maior força também é sua maior fraqueza, Mayumi sofre de daltonismo, especificamente acromatopsia, um tipo extremamente raro, acredita-se que ela viveu no Período 600–1000 d.C. (período Asuka a Heian, Japão), não se sabe ao certo, será que isso lhe impede de ver o fim?",
       cardFoco: "Deck de cores, focado em troca de modos de combate e combo.",
       hp: 85,
       maxHp: 85,
@@ -322,15 +322,15 @@ function configurarSelecaoPersonagem() {
       nome: "☀️Cleopatra VIII, A Reencarnação Divina☀️",
       dificuldade: "☀️NÍVEL DE DIFICULDADE: ☠️☠️☠️☠️☠️☀️",
       cor: "rgb(141, 7, 231)",
-      descricao: "...",
-      cardFoco: "...",
+      descricao: "Seu nome original era Aparna, mas após um ocorrido que ela diz ser a \"escolhida\", ela se auto intitulou Cleopatra VIII, e recebe ajuda dos deuses antigos do egito para ajudar ela em sua jornada, sendo capaz de os invocar e até entrar em estado divino para lutar.",
+      cardFoco: "Deck focado em spawn de aliados, buff de aliados e geração de cargas.",
       hp: 100,
       maxHp: 100,
       energia: 5,
       escudo: 0,
       mao: 6,
       img: "./../img/jogo/player/animado/cleopatra/statico/cleopatra1.png",
-      lor: "...",
+      lor: "Aparna sempre foi apaixonada por animes. Seu maior sonho? Viajar entre realidades, provar comidas impossíveis… e, por algum motivo, dirigir um caminhão.<br><br>Mas sonhos curiosos costumam levar a caminhos perigosos.<br><br>Em um dia aparentemente comum, ela decidiu explorar uma área da cidade que sua família sempre proibiu — um lugar abandonado, esquecido… e temido. A curiosidade falou mais alto.<br><br>Dois minutos.<br>Foi o tempo que levou para tudo dar errado.<br><br>Ela foi cercada.<br>Homens desconhecidos, olhares frios, intenções claras.<br><br>Aparna correu.<br>Desviou.<br>Lutou como pôde.<br><br>Mas não saiu ilesa.<br>Um corte profundo rasgou sua pele, e o sangue começou a escorrer sem parar.<br><br>Com a visão ficando turva, ela encontrou refúgio em uma fábrica abandonada.<br>Ou pelo menos… era o que parecia.<br><br>Lá dentro, havia pessoas.<br>Cientistas.<br>Guardas.<br>E um símbolo… o mesmo dos homens que a perseguiam.<br><br>Ela se escondeu e escutou.<br><br>Eles falavam sobre algo impossível.<br>Sobre trazer de volta uma divindade…<br>Em um corpo robótico.<br><br>Assim como haviam feito com uma IA.<br><br>Mesmo fraca, sangrando, quase desmaiando… Aparna queria ver mais.<br><br>Subiu pelas vigas enferrujadas no topo da fábrica.<br>Cada movimento era dor.<br>Cada segundo, um risco.<br><br>E então… ela viu.<br><br>Um corpo metálico…<br>Majestoso…<br>Imponente…<br><br>Sem dúvidas… inspirado em Cleopatra VII.<br><br>Aparna ficou fascinada.<br><br>Até que…<br>Uma gota de sangue caiu.<br><br>Direto na cabeça de um dos guardas.<br><br>Silêncio.<br><br>— Ela está ali!<br><br>Tudo aconteceu rápido.<br>Os cientistas entraram em desespero.<br>A máquina precisava ser ativada imediatamente.<br>Uma tempestade de raios rugia do lado de fora.<br><br>Os guardas começaram a subir.<br><br>Aparna… não aguentava mais.<br><br>A perda de sangue venceu.<br><br>E ela caiu.<br><br>Direto… sobre o corpo da divindade mecânica.<br><br>Escuridão.<br><br>...<br><br>Quando abriu os olhos…<br>o mundo havia mudado.<br><br>A fábrica estava destruída.<br>Reduzida a cinzas.<br><br>E então…<br>ela ouviu uma voz.<br><br>— Aparna...<br><br>— Quem está aí?! Eu não tenho medo!<br><br>— Cleopatra VII. Eu escolhi você… como minha herdeira espiritual.<br><br>— ...o quê?<br><br>— Seu sacrifício foi belo. Você entregou sua vida tentando me trazer de volta a este mundo.<br><br>— Ah… claro… era exatamente isso que eu tava fazendo...<br><br>— Eu sei que estava. E por isso… concedo a você meus poderes… e o auxílio dos deuses do Egito, para erguer novamente nosso glorioso império.<br><br>— Tá… mas… que poderes são esses?<br><br>— Muitos. Eles variam… de Cleopatra para Cleopatra.<br><br>Aparna, em pensamento, só conseguia repetir:<br>'por favor que seja um stand… por favor que seja um stand…'<br><br>— Você precisará descobrir sozinha como usá-los…<br>Mas pode invocar os deuses… sempre que precisar.<br><br>Com um leve deboche, Aparna responde:<br><br>— Ah claro… então Anubis, aparece aí pra gente trocar uma ideia.<br><br>Das cinzas…<br>algo surgiu.<br><br>Um robô.<br>Com a forma de Anubis.<br><br>— Aqui estou, Vizir.<br><br>Aparna ficou em choque.<br><br>— Ué… então os deuses são robôs?!<br><br>— Somos seres divinos… incompreendidos por sua visão limitada.<br><br>— ...ah…<br><br>— Assumi a forma que seu coração desejava.<br><br>O silêncio durou um segundo.<br><br>Então…<br>Aparna começou a pular de alegria.<br><br>Ela caiu de joelhos.<br>Agradeceu.<br><br>— Eu vou honrar isso… eu juro!<br><br>Decidiu naquele momento:<br>Seu nome também seria Cleopatra.<br><br>Não apenas como homenagem…<br>Mas como renascimento.<br><br>E assim…<br>ela partiu em sua jornada.<br><br>Para descobrir seus poderes.<br>Seu destino.<br><br>E talvez…<br>finalmente…<br>viajar entre dimensões.",
       extras: [
         {
           img: "./../img/jogo/cards/desert/era.png",
